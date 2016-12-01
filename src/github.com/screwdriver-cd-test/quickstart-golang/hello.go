eaf7f54dacf675d6083c55cdf63c4b10c512eece
Darren M <aeneascorrupt@gmail.com>
2016-12-01 12:30:49 -0800

    include minimalistic Golang library

---
+++ src/github.com/screwdriver-cd-test/quickstart-golang/hello.go
@@ -0,0 +1,5 @@
+package hello
+
+func greetings() string {
+	return "Hello, Go"
+}
---
+++ src/github.com/screwdriver-cd-test/quickstart-golang/hello_test.go
@@ -0,0 +1,15 @@
+package hello
+
+import (
+	"strings"
+	"testing"
+)
+
+func TestGreetings(t *testing.T) {
+    expected := "Hello, Go"
+	result := greetings()
+
+	if !strings.EqualFold(result, expected) {
+		t.Errorf("result == %q, want %q", result, expected)
+	}
+}
